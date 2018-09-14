70190d7dc8f4befb4fb20655e2c3263d798f2df1
Pål Larsen <paaledward@gmail.com>
2018-09-14 16:00:53 +0200

    Adds the ImageTabs class to the site

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
+import ImageTabs from './ImageTabs.jsx'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-            Müller er dum 
-        </p>
+        <h1>Title of thing</h1>
+        <ImageTabs />
       </div>
     );
   }
