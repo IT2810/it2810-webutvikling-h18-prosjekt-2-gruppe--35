de35ecabcdddf797e93fcc93a83fca7082db3aea
Pål Larsen <paaledward@gmail.com>
2018-09-18 17:41:28 +0200

    Adds functionality to render text and an image by selecting two checkboxes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import ImageTabs from './components/ImageTabs.jsx';
+import ButtonTabs from './components/ButtonTabs.jsx';
 import CheckboxTabs from './components/CheckboxTabs.jsx';
+import ContentBox from './parts/ContentBox.jsx';
 
 const imageDict = {
-                    Animals:'./resources/image/dyr',
-                    BowtieLad:'./resources/image/sloyfegutt',
-                    BlackAndWhite:'./resources/image/svarthvitt'};
+                    Animals:'/resources/images/dyr',
+                    BowtieLad:'/resources/images/sloyfegutt',
+                    BlackAndWhite:'/resources/images/svarthvitt'};
 
-const textDict = {  OwenWilson: './resources/json/owenWilson.json',
-                    RomanticNationalism: './resources/json/romanticNationalism.json',
-                    Shakespeare: './resources/json/shakespeare.json'};
+const textDict = {  OwenWilson: '/resources/json/owenWilson.json',
+                    RomanticNationalism: 'resources/json/romanticNationalism.json',
+                    Shakespeare: '/resources/json/shakespeare.json'};
 
 const soundDict = {};
 
@@ -19,12 +20,12 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            textCategory:null,
-            textNumber:null,
+            text:null,
+            textNumber:0,
             imageCategory:null,
-            imageNumber:null,
-            soundCategory:null,
-            soundNumber:null,
+            imageUrl:null,
+            soundUrl:null,
+            soundNumber:0,
         }
     }
 
@@ -33,19 +34,21 @@ class App extends Component {
         let resourceUrl;
         switch(category) {
             case 'image':
-                this.setState({imageCategory:category});
                 resourceUrl = this.iterateDictionary(id, imageDict);
+                this.setState({imageCategory:resourceUrl});
                 break;
             case 'text':
-                this.setState({textCategory:category});
                 resourceUrl = this.iterateDictionary(id, textDict);
+                this.textFetching(resourceUrl);
                 break;
             case 'sound':
-                this.setState({soundCategory:category});
                 resourceUrl = this.iterateDictionary(id, soundDict);
+                this.setState({soundUrl:resourceUrl});
+                break;
+            default:
+                this.setState({imageCategory:null, text:null, soundUrl:null});
                 break;
         }
-        console.log(resourceUrl);
     }
 
     iterateDictionary(id, dict) {
@@ -56,11 +59,77 @@ class App extends Component {
         }
     }
 
+    textFetching(resourceUrl) {
+        if (!this.caching(resourceUrl)) {
+            resourceUrl = new Promise ((resolve) => resolve(fetch(resourceUrl).then(res => res.json())));
+            Promise.all([resourceUrl]).then((result) => this.textParseJson(result[0]));
+        }
+    }
+
+    textParseJson(textObject) {
+        let textPElements = [];
+        for (const poemNr in textObject) {
+            textPElements.push(<p key={poemNr}>{textObject[poemNr]}</p>);
+        }
+        const textDiv = (<div>{textPElements}</div>);
+        const textPromise = new Promise ((resolve) => resolve(this.setState({text:textDiv})));
+    }
+
+    textGetPoemDiv(index) {
+        for (const lineNr in this.state.text) {
+            const pElement = (<p>{this.state.text[lineNr]}</p>);
+            console.log(pElement);
+        }
+    }
+
+    caching(resour) {
+        return false;
+    }
+
+    getImageIndex(index) {
+        index = parseInt(index, 10);
+        let url;
+        switch(index) {
+            case 0:
+                break;
+            case 1:
+                url = "/img1.jpg";
+                break;
+            case 2:
+                url = "/img2.jpg";
+                break;
+            case 3:
+                url = "/img3.jpg";
+                break;
+            case 4:
+                url = "/img4.jpg";
+                break;
+            default:
+                url = "/";
+        }
+        this.setState({imageUrl:url});
+    }
+
+    getImageUrl() {
+        const categoryUrl = this.state.imageCategory;
+        const imageUrl = this.state.imageUrl;
+        return (categoryUrl + imageUrl);
+    }
+
+    renderContentBox() {
+        if (this.state.text !== null && this.state.getImageUrl !== null) {
+            return (<ContentBox textDiv={this.state.text} imageUrl={this.getImageUrl()}/>);
+        } else {
+            return false;
+        }
+    }
+
     render() {
         return (
           <div className="App">
             <h1>Title of thing</h1>
-            <ImageTabs />
+            <ButtonTabs onSelected={this.getImageIndex.bind(this)} />
+            {this.renderContentBox()}
             <CheckboxTabs selectedCategory={this.pushSelectedCategory.bind(this)} />
           </div>
         );
