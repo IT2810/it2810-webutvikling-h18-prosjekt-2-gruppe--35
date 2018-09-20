e79c29e68b08236d2d417e1ac002822ec76aaa83
Pål Larsen <paaledward@gmail.com>
2018-09-20 16:13:43 +0200

    Revert "Added a homepage <p> element"
    This reverts commit 4246e205d68137449622df96c682c9735340e862.

--- src/parts/ContentBox.jsx
+++ src/parts/ContentBox.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 import Image from '../parts/Image.jsx';
 
-const homePage = <p>This is the homepage. To view other things you have to select
-                    one of each type of category. That is one category of texts,
-                    one category of images, and one category of sound types.</p>
-
 class ContentBox extends Component {
 
     render() {
-        if (this.props.index === 0 || this.props.index === null) {
-            return homePage;
-        } else {
-            return (
-                <React.Fragment>
-                    <Image imageUrl={this.props.imageUrl}/>
-                    <div>{this.props.textDiv}</div>
-                </React.Fragment>
-            );
-        }
+        return (
+            <React.Fragment>
+                <Image imageUrl={this.props.imageUrl}/>
+                <div>{this.props.textDiv}</div>
+            </React.Fragment>
+        );
     }
 }
 
