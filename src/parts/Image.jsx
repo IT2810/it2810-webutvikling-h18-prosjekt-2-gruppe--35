a2216edcba06315eb66cc3fcc6349a62d0a1dde3
Pål Larsen <paaledward@gmail.com>
2018-09-20 20:00:14 +0200

    removed log and constructor

--- src/parts/Image.jsx
+++ src/parts/Image.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 
 class Image extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
-        console.log(this.props.image);
         return (
         <div className="graphic" dangerouslySetInnerHTML={{__html:this.props.image}}>
         </div>
