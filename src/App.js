bf640a33bc6b5a9899a7096907eef07bf0e8e791
Pål Larsen <paaledward@gmail.com>
2018-09-20 17:23:59 +0200

    A lot of changes to fetch json and load the correct things on a button push

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import ButtonTabs from './components/ButtonTabs.jsx';
 import CheckboxTabs from './components/CheckboxTabs.jsx';
-import ContentBox from './parts/ContentBox.jsx';
+import ContentBox from './components/ContentBox.jsx';
 import "./App.css";
 
 const imageDict = {
-                    Animals:'/resources/images/dyr',
-                    BowtieLad:'/resources/images/sloyfegutt',
-                    BlackAndWhite:'/resources/images/svarthvitt'};
+                    Animals:'/resources/images/animals',
+                    Gems:'/resources/images/gems',
+                    BlackAndWhite:'/resources/images/black&white'};
 
-const textDict = {  OwenWilson: '/resources/json/wilfredOwen.json',
+const textDict = {  WilfredOwen: '/resources/json/wilfredOwen.json',
                     RomanticNationalism: 'resources/json/romanticNationalism.json',
                     Shakespeare: '/resources/json/shakespeare.json'};
 
@@ -21,27 +21,29 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
+            textObject:null,
             text:null,
-            textNumber:null,
+            index:0,
             imageCategory:null,
+            textCategory:null,
             imageUrl:null,
             soundUrl:null,
             soundNumber:0,
         }
     }
 
-    pushSelectedCategory(id, category) {
+    updateCategory(id, category) {
         id = id.replace(/ /g, '');
         let resourceUrl;
         switch(category) {
             case 'image':
                 resourceUrl = this.iterateDictionary(id, imageDict);
-                console.log("image");
                 this.setState({imageCategory:resourceUrl});
                 break;
             case 'text':
                 resourceUrl = this.iterateDictionary(id, textDict);
-                this.textFetching(resourceUrl);
+                this.setState({textCategory:resourceUrl});
+                console.log(this.state.textCategory);
                 break;
             case 'sound':
                 resourceUrl = this.iterateDictionary(id, soundDict);
@@ -51,6 +53,7 @@ class App extends Component {
                 this.setState({imageCategory:null, text:null, soundUrl:null});
                 break;
         }
+        this.renderUpdate(this.state.index);
     }
 
     iterateDictionary(id, dict) {
@@ -61,69 +64,53 @@ class App extends Component {
         }
     }
 
-    textFetching(resourceUrl) {
+    fetchJsonPromise() {
+        const resourceUrl = this.state.textCategory;
         if (!this.caching(resourceUrl)) {
-            resourceUrl = new Promise ((resolve) => resolve(fetch(resourceUrl).then(res => res.json())));
-            Promise.all([resourceUrl]).then((result) => this.textParseJson(result[0]));
+            const jsonPromise = new Promise ((resolve) =>
+                resolve(fetch(resourceUrl)
+                    .then(res => res.json())));
+            return jsonPromise;
         }
     }
 
-    textParseJson(textObject) {
-        let textPElements = [];
+    fetchJson(resourceUrl) {
+        return fetch(resourceUrl).then(res => res.json());
+    }
+
+
+    getCorrectPoem(index, textObject) {
+        const textPElements = [];
         for (const poemNr in textObject) {
-            textPElements.push(<p key={poemNr}>{textObject[poemNr]}</p>);
+            if (index === parseInt(poemNr, 10)) {
+                for (const lineNr in textObject[poemNr]) {
+                        textPElements.push(<p>{textObject[poemNr][lineNr]}</p>);
+                }
+            }
         }
-        const textDiv = (<div>{textPElements}</div>);
-        const textPromise = new Promise ((resolve) => resolve(this.setState({text:textDiv})));
-    }
-
-    textGetPoemDiv(index) {
-        for (const lineNr in this.state.text) {
-            const pElement = (<p>{this.state.text[lineNr]}</p>);
-            console.log(pElement);
-        }
+        return (<div>{textPElements}</div>);
     }
 
     caching(resour) {
         return false;
     }
 
-    getImageIndex(index) {
+    renderUpdate(index) {
         index = parseInt(index, 10);
-        let url;
-        switch(index) {
-            case 0:
-                break;
-            case 1:
-                url = "/img1.jpg";
-                break;
-            case 2:
-                url = "/img2.jpg";
-                break;
-            case 3:
-                url = "/img3.jpg";
-                break;
-            case 4:
-                url = "/img4.jpg";
-                break;
-            default:
-                url = "/";
-        }
-        this.setState({imageUrl:url});
+        let text;
+        const imageUrl = this.updateImage(index);
+        const promise = this.fetchJsonPromise();
+        Promise.all([promise]).then((result) => {
+            text = this.getCorrectPoem(index, result[0]);
+            console.log(text);
+            this.setState({imageUrl:imageUrl, text:text, index:index});
+        });
+
     }
 
-    getImageUrl() {
-        const categoryUrl = this.state.imageCategory;
-        const imageUrl = this.state.imageUrl;
-        return (categoryUrl + imageUrl);
-    }
-
-    renderContentBox() {
-        if (this.state.text !== null && this.state.imageUrl !== null && this.state.imageCategory !== null) {
-            return (<ContentBox textDiv={this.state.text} imageUrl={this.getImageUrl()}/>);
-        } else {
-            return false;
-        }
+    updateImage(index) {
+        const url = "/img" + index + ".svg";
+        return url;
     }
 
     render() {
@@ -131,11 +118,34 @@ class App extends Component {
           <div className="App">
             <h1>Title of thing</h1>
             <div className="container">
-                <ButtonTabs className="ImageTabs" onSelected={this.getImageIndex.bind(this)} />
+                <ButtonTabs className="ImageTabs" onSelected={this.renderUpdate.bind(this)} />
                 {this.renderContentBox()}
-                <CheckboxTabs className="CheckboxTabs" selectedCategory={this.pushSelectedCategory.bind(this)} />
+                <CheckboxTabs className="CheckboxTabs" selectedCategory={this.updateCategory.bind(this)} />
             </div>          </div>
         );
+    }
+
+    renderContentBox() {
+        if (this.canRender()) {
+            return (<ContentBox index={this.state.index}
+                textDiv={this.state.text}
+                imageUrl={this.getImageUrl()}/>);
+        } else {
+            return <ContentBox index={this.state.index} />;
+        }
+    }
+
+    canRender() {
+        return (this.props.imageCategory !== null
+                && this.props.soundCategory !== null
+                && this.props.textCategory !== null)
+                && this.props.index !==0;
+    }
+
+    getImageUrl() {
+        const categoryUrl = this.state.imageCategory;
+        const imageUrl = this.state.imageUrl;
+        return (categoryUrl + imageUrl);
     }
 }
 
