11dbea6933e0a7d21f91088a846ff5dda789144c
Pål Larsen <paaledward@gmail.com>
2018-09-20 18:37:51 +0200

    Fixes that the audio tag would load when it was undefined

--- src/parts/Audio.jsx
+++ src/parts/Audio.jsx
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 class Audio extends Component {
     render() {
-        return (
-            <audio key={this.props.soundUrl} controls>
+        if (this.props.soundUrl !== undefined) {
+            return (
+                <audio key={this.props.soundUrl} controls>
                     <source src={this.props.soundUrl}/>
-                        Your browser does not support the audio element.
-            </audio>
-        );
+                    Your browser does not support the audio element.
+                </audio>
+            );
+        } else {
+            return false;
+        }
     }
 }
 
