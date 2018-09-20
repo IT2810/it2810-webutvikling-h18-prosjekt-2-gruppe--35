80db95eae3dfff46fbc9a62ab443fd9f06e4ace6
Pål Larsen <paaledward@gmail.com>
2018-09-20 19:25:15 +0200

    The images are now properly fetched as xml and laoded

--- src/parts/Image.jsx
+++ src/parts/Image.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
 class Image extends Component {
-    fetchSvg() {
-        return false;
+    constructor(props) {
+        super(props);
+        this.state = {
+            image:null,
+        }
+    }
+
+    fetchSvg(imageUrl) {
+        console.log(imageUrl);
+        const imagePromise = new Promise((resolve) => resolve(
+            fetch(imageUrl)
+                .then(res => res.text())
+                //.then(result => console.log(result))
+            ));
+
+        Promise.all([imagePromise]).then(result => {
+            console.log(result);
+            this.setState({image:result[0]});
+        });
     }
 
     render() {
-        return this.fetchSvg()
+        this.fetchSvg(this.props.imageUrl);
+
+        return (
+        <div className="graphic" dangerouslySetInnerHTML={{__html:this.state.image}}>
+        </div>
+        );
     }
 }
 
