1d1cb0f3af706f14ba7f5aa7bc4476d75a1ce33d
Pål Larsen <paaledward@gmail.com>
2018-09-20 17:23:05 +0200

    Made the creation of buttons dynamic

--- src/components/ButtonTabs.jsx
+++ src/components/ButtonTabs.jsx
@@ -14,14 +14,20 @@ class ButtonTabs extends Component {
         this.props.onSelected(index);
     }
 
+    createButtons() {
+        const buttons = [];
+        const buttonNames = ["Home", "One", "Two", "Three", "Four"];
+        const numberOfButtons = buttonNames.length
+        for (let button = 0; button < numberOfButtons; button++) {
+            buttons.push(<Button handleClick={this.buttonClick.bind(this)} name={buttonNames[button]} index={button} activeButton={this.state.activeButton} />);
+        }
+        return buttons;
+    }
+
     render() {
             return (
                 <div>
-                    <Button name="Home" handleClick={this.buttonClick.bind(this)} index="0" activeButton={this.state.activeButton}/>
-                    <Button name="One" handleClick={this.buttonClick.bind(this)} index="1" activeButton={this.state.activeButton}/>
-                    <Button name="Two" handleClick={this.buttonClick.bind(this)} index="2" activeButton={this.state.activeButton}/>
-                    <Button name="Three" handleClick={this.buttonClick.bind(this)} index="3" activeButton={this.state.activeButton}/>
-                    <Button name="Four" handleClick={this.buttonClick.bind(this)} index="4" activeButton={this.state.activeButton}/>
+                    {this.createButtons()}
                 </div>
             );
         }
